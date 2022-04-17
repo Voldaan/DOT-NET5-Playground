e4a71f9aeafb1364b85739a60afb45b935abbe2e
Voldaan <autodropcadillac@gmail.com>
2022-04-17 14:52:59 +0200

    Added seeding for the Artists table

--- MVC-Frontend-and-REST-API/MVC-Frontend-and-REST-API/Data/DataContext.cs
+++ MVC-Frontend-and-REST-API/MVC-Frontend-and-REST-API/Data/DataContext.cs
@@ -8,10 +8,38 @@ using System.Threading.Tasks;
 
 namespace MVC_Frontend_and_REST_API.Data
 {
-    public class DataContext : IdentityDbContext
+    public class DataContext : DbContext //IdentityDbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Artist>().HasData(
+            #region Artist seed
+                new Artist
+                {
+                    Id = new Guid("E99DC311-0C6E-40F0-B97D-755135ECFC4A"),
+                    Name = "Carpenter Brut",
+                    Description = "Franck Hueso, better known by his stage name Carpenter Brut, is a French darksynth artist from Poitiers, France. Carpenter Brut claims his relative anonymity is a deliberate artistic choice in order to place more importance on the music itself, rather than the identity of the musician behind it. He started writing music as Carpenter Brut with the intention of mixing sounds from horror films, metal, rock, and electronic music. In live performances Carpenter Brut is joined on stage by guitarist Adrien Grousset and drummer Florent Marcadet, both from the French metal band Hacride and in 2016 Brut toured the United States with the Swedish heavy metal band Ghost."
+                },
+                new Artist
+                {
+                    Id = new Guid("AE2F32B7-68A2-4DA9-A53B-F9ED8A604D1C"),
+                    Name = "Three Days Grace",
+                    Description = "Three Days Grace is a Canadian rock band formed in Norwood, Ontario in 1997. The band's original iteration was called \"Groundswell\" and played in various local Norwood back-yard parties and area establishments from 1993 to 1996. Based in Toronto, the band's original line-up consisted of guitarist and lead vocalist Adam Gontier, drummer and backing vocalist Neil Sanderson, and bassist Brad Walst. In 2003, Barry Stock was recruited as the band's lead guitarist, making them a four - member band.In 2013, Gontier left the band and was replaced by My Darkest Days' vocalist Matt Walst, who is also the younger brother of Brad Walst."
+                },
+                new Artist
+                {
+                    Id = new Guid("6093964A-D53F-46FE-9D20-AEB9CA3F2FF4"),
+                    Name = "If These Trees Could Talk",
+                    Description = "If These Trees Could Talk is an instrumental post-rock band from Akron, Ohio. The band self-released their self-titled debut EP in 2006. Independent record label The Mylene Sheath re-released the EP on vinyl in 2007, and went on to release the band's debut studio album, Above the Earth, Below the Sky, on vinyl also, in 2009. The band self-released their second album Red Forest in March 2012, whilst the album's vinyl release went through Science of Silence Records. They went on to follow up the release of \"Red Forest\" with a self-promoted tour throughout Europe in April 2012. The band released its third album, The Bones of a Dying World, in June, 2016 on Metal Blade Records."
+                }
+                #endregion
+            );
+        }
+
         public DbSet<Artist> Artists { get; set; }
+
+        
     }
 }
--- MVC-Frontend-and-REST-API/MVC-Frontend-and-REST-API/Models/DataModels/Artist.cs
+++ MVC-Frontend-and-REST-API/MVC-Frontend-and-REST-API/Models/DataModels/Artist.cs
@@ -14,7 +14,7 @@ namespace MVC_Frontend_and_REST_API.Models.DataModels
         [MaxLength(50, ErrorMessage = "A maximum of {1} characters allowed")]      //This is called DataAnnotations
         public string Name { get; set; }
         [Required]
-        [MaxLength(500, ErrorMessage = "A maximum of {1} characters allowed")]
+        [MaxLength(1000, ErrorMessage = "A maximum of {1} characters allowed")]
         public string Description { get; set; }
     }
 }
